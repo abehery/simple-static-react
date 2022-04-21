b37a50b383a29201b428a68bdaa339f4100aead3
abehery <a.mhbehery@gmail.com>
2022-04-21 14:04:50 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
 export default {
-  helloWorldData: 'A bunch of hello world data.'
+  helloWorldData: 'A bunch of hello world data.auto auto'
 , helloAgainWorldData: 'The page changed, but so did the data.'
 }
