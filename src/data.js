6907111e8a7bf32cf6e61e2a3e0e8a4f61794f12
abehery <a.mhbehery@gmail.com>
2022-04-21 14:15:07 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
 export default {
-  helloWorldData: 'A bunch of hello world data.Stop'
+  helloWorldData: 'A bunch of hello world data. webhook'
 , helloAgainWorldData: 'The page changed, but so did the data.'
 }
